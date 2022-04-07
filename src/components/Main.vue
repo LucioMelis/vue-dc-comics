f91a7c38f623b3c49f05caefe00c89729fef7ac3
luciomelis <luciomelis1992@gmail.com>
2022-04-07 20:30:36 +0200

    Esercizio Terminato

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,27 +1,79 @@
 <template>
-  <div class="container-main">
-    <div class="content">-- Content goes here --</div>
+  <div>
+    <!-- container-Jumbotron  -->
+    <div class="container-bg_main-top">
+      <div class="label-main-top">CURRENT SERIES</div>
+    </div>
+    <!-- container Comics  -->
+    <div class="container-bg-main-bottom">
+      <div class="content-dc-series">
+        <SingoloFumetto
+          v-for="(fumetto, index) in fumettiDc"
+          :key="index"
+          :fumetto="fumetto"
+        />
+      </div>
+      <div class="container-button">
+        <button>LOAD MORE</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import fumettiDc from "@/assets/data-json/dc-comics";
+import SingoloFumetto from "@/components/SingoloFumetto.vue";
+
 export default {
   name: "Main_DC",
+  data() {
+    return {
+      fumettiDc,
+    };
+  },
+  components: {
+    SingoloFumetto,
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import "src/variabili.scss";
-.container-main {
+.container-bg_main-top {
+  background-image: url("@/assets/jumbotron.jpg");
+  height: 380px;
+  background-size: cover;
+  position: relative;
+  .label-main-top {
+    background-color: $color-primary;
+    padding: 10px 20px;
+    color: white;
+    position: absolute;
+    bottom: -20px;
+    left: 200px;
+  }
+}
+.container-bg-main-bottom {
   background-color: $color-dark;
-  .content {
+  .content-dc-series {
     width: $container-principal;
     margin: $margin-container;
-    color: white;
-    padding: 40px 0;
-    font-weight: bold;
-    font-size: 1.2rem;
+    padding: 30px 0;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 15px;
+  }
+  .container-button {
+    display: flex;
+    justify-content: center;
+    button {
+      color: white;
+      font-weight: bold;
+      background-color: $color-primary;
+      padding: 10px 30px;
+      margin: 20px 0;
+    }
   }
 }
 </style>
---
+++ src/components/SingoloFumetto.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card-fumetto">
+    <img :src="fumetto.thumb" alt="fumetto" />
+    <p>{{ fumetto.series }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SingoloFumetto",
+  props: {
+    fumetto: Object,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "src/variabili.scss";
+.card-fumetto {
+  width: calc(100% / 6 - 15px * 5 / 6);
+  img {
+    width: 100%;
+  }
+  p {
+    color: white;
+    font-size: 12px;
+    padding: 10px 0;
+  }
+}
+</style>
